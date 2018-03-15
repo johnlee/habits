841c5cc70eec14e28fc5ef0571c352c8729deebc
John Lee <lee1048@llnl.gov>
2018-03-15 15:46:09 -0700

    added chartjs

--- script.js
+++ script.js
@@ -10,141 +10,207 @@ $(document).ready(function () {
     $("editform").submit(function (e) { e.preventDefault(); });
 
     $("#btnExtinguish").click(function () {
-      if (dateCheck()) { submitAction("provoke"); }
+        if (dateCheck()) { submitAction("provoke"); }
     });
 
     $("#btnBurn").click(function () {
-      if (dateCheck()) { submitAction("succumb"); }
+        if (dateCheck()) { submitAction("succumb"); }
     });
 
     $("#btnSave").click(function () {
-      if (dateCheck()) { 
-        var content = {
-          provoke: $("#editExtinguish").val(),
-          succumb: $("#editFire").val()
-        };
-        $.ajax({
-          url: url + '/' + $("#editDate").val(),
-          dataType: "json",
-          contentType: "application/json;charset=utf-8",
-          type: "POST",
-          data: JSON.stringify(content),
-          success: function (result) {
-            success(result);
-          },
-          error: function (status) {
-            if (status && status.status && status.status == 200) {
-              success(status);
-            } else {
-              alert("ERROR - unable to submit request. " + JSON.stringify(status));
-              console.log(JSON.stringify(status));
-            }
-          }
-        });
-      }
+        if (dateCheck()) {
+            var content = {
+                provoke: $("#editExtinguish").val(),
+                succumb: $("#editFire").val()
+            };
+            $.ajax({
+                url: url + '/' + $("#editDate").val(),
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                type: "POST",
+                data: JSON.stringify(content),
+                success: function (result) {
+                    success(result);
+                },
+                error: function (status) {
+                    if (status && status.status && status.status == 200) {
+                        success(status);
+                    } else {
+                        alert("ERROR - unable to submit request. " + JSON.stringify(status));
+                        console.log(JSON.stringify(status));
+                    }
+                }
+            });
+        }
     });
 
     function submitAction(action) {
-      var content = {
-        action: action,
-        date: $("#formDate").val()
-      };
-      $.ajax({
-        url: url,
-        dataType: "json",
-        contentType: "application/json;charset=utf-8",
-        type: "POST",
-        data: JSON.stringify(content),
-        success: function (result) {
-          success(result);
-        },
-        error: function (status) {
-          if (status && status.status && status.status == 200) {
-            success(status);
-          } else {
-            alert("ERROR - unable to submit request. " + JSON.stringify(status));
-            console.log(JSON.stringify(status));
-          }
-        }
-      });
+        var content = {
+            action: action,
+            date: $("#formDate").val()
+        };
+        $.ajax({
+            url: url,
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            type: "POST",
+            data: JSON.stringify(content),
+            success: function (result) {
+                success(result);
+            },
+            error: function (status) {
+                if (status && status.status && status.status == 200) {
+                    success(status);
+                } else {
+                    alert("ERROR - unable to submit request. " + JSON.stringify(status));
+                    console.log(JSON.stringify(status));
+                }
+            }
+        });
     }
 
     function dateCheck() {
-      var date = $("#formDate").val();
-      if (date.length != 8) {
-        alert("ERROR - invalid date!");
-        return false;
-      }
-      return true;
+        var date = $("#formDate").val();
+        if (date.length != 8) {
+            alert("ERROR - invalid date!");
+            return false;
+        }
+        return true;
     }
 
     function dateToYMD() {
-      var date = new Date();
-      var d = date.getDate();
-      var m = date.getMonth() + 1; //Month from 0 to 11
-      var y = date.getFullYear();
-      return '' + y + (m <= 9 ? '0' + m : m) + (d <= 9 ? '0' + d : d);
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth() + 1; //Month from 0 to 11
+        var y = date.getFullYear();
+        return '' + y + (m <= 9 ? '0' + m : m) + (d <= 9 ? '0' + d : d);
     }
 
     function drawFire(count) {
-      var fire = `<i class="fas fa-fire"></i> `;
-      var result = '';
-      for (i = 0; i < count; i++) {
-        result += fire;
-      }
-      return result;
+        var fire = `<i class="fas fa-fire"></i> `;
+        var result = '';
+        for (i = 0; i < count; i++) {
+            result += fire;
+        }
+        return result;
     }
 
     function drawExtinguisher(count) {
-      var fire = `<i class="fas fa-fire-extinguisher"></i> `;
-      var result = '';
-      for (i = 0; i < count; i++) {
-        result += fire;
-      }
-      return result;
+        var fire = `<i class="fas fa-fire-extinguisher"></i> `;
+        var result = '';
+        for (i = 0; i < count; i++) {
+            result += fire;
+        }
+        return result;
     }
 
     function success(result) {
-      console.log("Successfully submitted request. " + JSON.stringify(result));
-      location.reload(true);
+        console.log("Successfully submitted request. " + JSON.stringify(result));
+        location.reload(true);
     }
 
     function loadPage() {
-      console.log('Loading Page...');
-      $.getJSON(url, function (data) {
-        var success = 0;
-        var fail = 0;
-        var totalExtinguish = 0;
-        var totalBurn = 0;
-        var days = 0;
+        console.log('Loading Page...');
+        var totalSuccess = 0;
+        var totalFail = 0;
+        var totalExtinguish =0;
+        var totalBurn =0;
+        var totalDays = 0;
+        var percentFail = 0;
+        var percentSuccess = 0;
+        var extinguishes = [];
+        var burns = [];
 
-        $.each(data, function (i, item) {
-          var extinguish = parseInt(item.provoke);
-          var burn = parseInt(item.succumb);
-          var bkgd = 'bkgd-good';
-          if (burn > 2) {
-            fail++;
-            bkgd = 'bkgd-danger';
-          } else {
-            success++;
-          }
-          if (days < 8) { 
-            totalExtinguish = totalExtinguish + extinguish;
-            totalBurn = totalBurn + burn; 
-          }
-          days++;
+        $.getJSON(url, function (data) {
+            $.each(data, function (i, item) {
+                var extinguish = parseInt(item.provoke);
+                var burn = parseInt(item.succumb);
+                var bkgd = 'bkgd-good';
+                if (burn > 2) {
+                    totalFail++;
+                    bkgd = 'bkgd-danger';
+                } else {
+                    totalSuccess++;
+                }
+                if (totalDays < 8) {
+                    totalExtinguish = totalExtinguish + extinguish;
+                    totalBurn = totalBurn + burn;
+                }
+                totalDays++;
+                extinguishes.push(extinguish);
+                burns.push(burn);
 
-          var row = `<tr><td class='${bkgd}'">${item.date}</td><td>${drawExtinguisher(extinguish)}</td><td>${drawFire(burn)}</td></tr>`;
-          $("#tableBody").append(row);
+                var row = `<tr><td class='${bkgd}'">${item.date}</td><td>${drawExtinguisher(extinguish)}</td><td>${drawFire(burn)}</td></tr>`;
+                $("#tableBody").append(row);
+            });
+            percentSuccess = totalSuccess / totalDays;
+            percentFail = totalFail / totalDays;
+            $("#totalSuccess").append(`<strong title="${percentSuccess}">${totalSuccess}</strong>`);
+            $("#totalFail").append(`<strong title="${percentFail}">${totalFail}</strong>`);
+            $("#totalExtinguished").append(`<strong title="In past 8 days">${totalExtinguish}</strong>`);
+            $("#totalBurned").append(`<strong">${totalBurn}</strong>`);
+            $("#totalDays").append(`<strong">${totalDays}</strong>`);
+            extinguishes = extinguishes.reverse();
+            burns = burns.reverse();
+            if (totalDays > 30) {
+                extinguishes = extinguishes.slice(totalDays - 30);
+                burns = burns.slice(totalDays - 30);
+            }
+            var line_config = {
+                type: 'line',
+                data: {
+                    labels: new Array(30),
+                    datasets: [
+                        {
+                            label: 'Extinguished',
+                            fill: false,
+                            backgroundColor: 'rgb(75, 192, 192)',
+                            data: extinguishes
+                        },
+                        {
+                            label: 'Burned',
+                            fill: true,
+                            backgroundColor: 'rgb(255, 99, 132)',
+                            data: burns
+                        }
+                    ]
+                },
+                options: {
+                    responsive: true,
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    }
+                }
+            }
+            var pie_config = {
+                type: 'pie',
+                data: {
+                    datasets: [{
+                        data: [totalSuccess, totalFail],
+                        backgroundColor: [
+                            'rgb(75, 192, 192)',
+                            'rgb(255, 99, 132)'
+                        ]
+                    }],
+                    labels: [
+                        'Success',
+                        'Fail'
+                    ]
+                },
+                options: {
+                    responsive: true
+                }
+            };    
+            var lineChart = document.getElementById('chart-line').getContext('2d');
+            window.myLine = new Chart(lineChart, line_config);
+            var pieChart = document.getElementById('chart-pie').getContext('2d');
+            window.myPie = new Chart(pieChart, pie_config);
         });
-        var percentSuccess = success / days;
-        var percentFail = fail / days;
-        $("#totalSuccess").append(`<strong title="${percentSuccess}">${success}</strong>`);
-        $("#totalFail").append(`<strong title="${percentFail}">${fail}</strong>`);
-        $("#totalExtinguished").append(`<strong title="In past 8 days">${totalExtinguish}</strong>`);
-        $("#totalBurned").append(`<strong">${totalBurn}</strong>`);
-        $("#totalDays").append(`<strong">${days}</strong>`);
-      });
     }
     loadPage();
-  });  
+});
